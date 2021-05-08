118019c0a6dd37f5cb716391d0892773833336fc
SenkJu <18741573+SenkJu@users.noreply.github.com>
2021-05-08 22:30:11 +0200

    workflow: #4 attempt to build on linux

--- tools/build/linux/build.sh
+++ tools/build/linux/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 curl -L https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage --output appimagetool-x86_64.AppImage
+chmod +x AppRun
 chmod a+x appimagetool-x86_64.AppImage
 cp -r ../../../public/* usr/bin
 ./appimagetool-x86_64.AppImage .
