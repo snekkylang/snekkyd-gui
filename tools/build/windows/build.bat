468e1cdea4cffc98545fe89851bba1e992cc49e5
SenkJu <18741573+SenkJu@users.noreply.github.com>
2021-05-08 22:47:37 +0200

    remove scapp from public folder

--- tools/build/windows/build.bat
+++ tools/build/windows/build.bat
@@ -1,5 +1,5 @@
 curl -L http://www.angusj.com/resourcehacker/resource_hacker.zip --output resource_hacker.zip
 7z e resource_hacker.zip
-7z a snekkyd-gui.7z ..\..\..\public\*
+7z a snekkyd-gui.7z scapp.exe ..\..\..\public\*
 copy /b 7zSD.sfx + sfx_config.txt + snekkyd-gui.7z "decompiler_sfx.exe"
 ResourceHacker.exe -script rh_script.txt
